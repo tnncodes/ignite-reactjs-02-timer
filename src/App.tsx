a4542621986366cab7939ca3d29f2f3f22292387
tnncodes <tnncodes@gmail.com>
2023-09-30 18:41:33 -0300

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./components/Button"
+
 export function App() {
   return (
-    <h1>hello world</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
 
