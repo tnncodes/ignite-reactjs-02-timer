57170292e926887390c5020c3bbcde1acf546ad1
tnncodes <tnncodes@gmail.com>
2023-10-01 15:45:34 -0300

    configuração do eslint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,16 +1,9 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export type ButtonVariant =  'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
-}
-
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green'
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
